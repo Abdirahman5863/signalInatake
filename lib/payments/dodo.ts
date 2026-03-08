b8cf46d2afaa1ce2a1c69dac7b036bf84908fd7f
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2026-03-08 13:12:39 +0300

    added live payments

--- lib/payments/dodo.ts
+++ lib/payments/dodo.ts
@@ -2,7 +2,7 @@ import DodoPayments from 'dodopayments'
 
 export const dodo = new DodoPayments({
   bearerToken: process.env.DODO_PAYMENTS_API_KEY!,
-  environment: 'test_mode',
+  environment: 'live_mode',
 })
 
 export const PRODUCT_IDS = {
