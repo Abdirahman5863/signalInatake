d41fb622d59be9091dc0fdd76885f195c2caf84c
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2026-03-15 13:38:34 +0300

    setting

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
   compress: true,
-  async rewrites() {
-    return [
-      {
-        source: '/supabase/:path*',
-        destination: 'https://flwsppivkfgzjwzlbssh.supabase.co/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
